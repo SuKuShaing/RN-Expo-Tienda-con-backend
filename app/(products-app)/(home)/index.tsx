a48e0e736c040f36405d3607139fb4b508c3de65
Sebastian <sebastian.sanhueza.t@usach.cl>
2025-12-09 14:26:13 -0300

    modificaciones iniciales para comenzar el proyecto

---
+++ app/(products-app)/(home)/index.tsx
@@ -0,0 +1,12 @@
+import { ThemedText } from "@/presentation/theme/components/themed-text";
+import { View } from "react-native";
+
+const HomeScreen = () => {
+    return (
+        <View>
+            <ThemedText>HomeScreen</ThemedText>
+        </View>
+    );
+};
+
+export default HomeScreen;
