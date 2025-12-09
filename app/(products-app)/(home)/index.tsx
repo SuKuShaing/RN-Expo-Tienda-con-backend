3e83b739c166bbcf4f685c8781c834c127a5566f
Sebastian <sebastian.sanhueza.t@usach.cl>
2025-12-09 14:53:42 -0300

    Se aplican cambios para que se pueda ver la fuente

--- app/(products-app)/(home)/index.tsx
+++ app/(products-app)/(home)/index.tsx
@@ -1,10 +1,22 @@
 import { ThemedText } from "@/presentation/theme/components/themed-text";
+import { useThemeColor } from "@/presentation/theme/hooks/use-theme-color";
 import { View } from "react-native";
 
 const HomeScreen = () => {
+    const primary = useThemeColor({}, "primary");
+
     return (
-        <View>
-            <ThemedText>HomeScreen</ThemedText>
+        <View style={{ paddingTop: 100, paddingHorizontal: 20 }}>
+            <ThemedText style={{ fontFamily: "KanitRegular" }}>
+                HomeScreen
+            </ThemedText>
+            <ThemedText style={{ fontFamily: "KanitBold" }}>
+                HomeScreen
+            </ThemedText>
+            <ThemedText style={{ fontFamily: "KanitThin" }}>
+                HomeScreen
+            </ThemedText>
+            <ThemedText style={{ color: primary }}>HomeScreen</ThemedText>
         </View>
     );
 };
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,13 @@
+import { useColorScheme } from "@/presentation/theme/hooks/use-color-scheme";
 import {
     DarkTheme,
     DefaultTheme,
     ThemeProvider,
 } from "@react-navigation/native";
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
-
-import { useColorScheme } from "@/presentation/theme/hooks/use-color-scheme";
 
 export const unstable_settings = {
     anchor: "(tabs)",
@@ -16,11 +16,21 @@ export const unstable_settings = {
 export default function RootLayout() {
     const colorScheme = useColorScheme();
 
+    const [loaded] = useFonts({
+        KanitRegular: require("../assets/fonts/Kanit-Regular.ttf"),
+        KanitBold: require("../assets/fonts/Kanit-Bold.ttf"),
+        KanitThin: require("../assets/fonts/Kanit-Thin.ttf"),
+    });
+
     return (
         <ThemeProvider
             value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
         >
-            <Stack>
+            <Stack
+                screenOptions={{
+                    headerShown: false,
+                }}
+            >
                 {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} /> */}
             </Stack>
