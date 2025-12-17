e85e7dc3b0814867ccc65f3589553699d102df0a
Sebastian <sebastian.sanhueza.t@usach.cl>
2025-12-17 14:00:06 -0300

    se crea el campo de texto

--- presentation/theme/components/ThemeTextInput.tsx
+++ presentation/theme/components/ThemeTextInput.tsx
@@ -1,16 +1,62 @@
 import { Ionicons } from "@expo/vector-icons";
-import { Text, TextInputProps, View } from "react-native";
+import { useRef, useState } from "react";
+import { StyleSheet, TextInput, TextInputProps, View } from "react-native";
+import { useThemeColor } from "../hooks/use-theme-color";
 
 interface Props extends TextInputProps {
     icon?: keyof typeof Ionicons.glyphMap;
 }
 
 const ThemeTextInput = ({ icon, ...rest }: Props) => {
+    const primaryColor = useThemeColor({}, "primary");
+    const textColor = useThemeColor({}, "text");
+
+    const [isActive, setIsActive] = useState(false);
+    const inputRef = useRef<TextInput>(null);
+
     return (
-        <View>
-            <Text>ThemeTextInput</Text>
+        <View
+            style={{
+                ...styles.border,
+                // ToDo: cambiar si tiene el foco el input
+                borderColor: isActive ? primaryColor : "#ccc",
+            }}
+            onTouchStart={() => inputRef.current?.focus()}
+        >
+            {icon && (
+                <Ionicons
+                    name={icon}
+                    size={24}
+                    color={textColor}
+                    style={{ marginRight: 10 }}
+                />
+            )}
+            <TextInput
+                ref={inputRef}
+                placeholderTextColor="#5C5C5C"
+                onFocus={() => setIsActive(true)}
+                onBlur={() => setIsActive(false)}
+                // onBlur es lo contrario a onFocus, es cuando pierde el foco
+                style={{
+                    color: textColor,
+                    marginRight: 10,
+                    flex: 1,
+                }}
+                {...rest}
+            />
         </View>
     );
 };
 
 export default ThemeTextInput;
+
+const styles = StyleSheet.create({
+    border: {
+        borderWidth: 1,
+        borderRadius: 5,
+        padding: 5,
+        marginBottom: 10,
+        flexDirection: "row",
+        alignItems: "center",
+    },
+});
