60c387bb0a7c345a251f2ee23e4b67750368153b
Sebastian <sebastian.sanhueza.t@usach.cl>
2026-02-12 12:32:24 -0300

    Diseño de los inputs en la pantalla del producto

--- app/(products-app)/product/[id].tsx
+++ app/(products-app)/product/[id].tsx
@@ -1,10 +1,60 @@
-import { Text, View } from "react-native";
+import { ThemedView } from "@/presentation/theme/components/themed-view";
+import ThemeTextInput from "@/presentation/theme/components/ThemeTextInput";
+import { Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "expo-router";
+import { useEffect } from "react";
+import { KeyboardAvoidingView, Platform, ScrollView } from "react-native";
 
 const ProductScreen = () => {
+    const navigation = useNavigation(); // son las opciones de la cabecera
+
+    useEffect(() => {
+        // ToDo: colocar el nombre del producto
+
+        navigation.setOptions({
+            headerRight: () => <Ionicons name="camera-outline" size={25} />,
+        });
+    }, []);
+
     return (
-        <View>
-            <Text>ProductScreen</Text>
-        </View>
+        <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : undefined}
+        >
+            <ScrollView>
+                {/* Todo: Product images */}
+                <ThemedView style={{ marginHorizontal: 10, marginTop: 20 }}>
+                    <ThemeTextInput
+                        placeholder="Título"
+                        // style={{ marginVertical: 5 }}
+                    />
+                    <ThemeTextInput
+                        placeholder="Slug"
+                        // style={{ marginVertical: 5 }}
+                    />
+                    <ThemeTextInput
+                        placeholder="Descripción"
+                        multiline
+                        numberOfLines={5}
+                        // style={{ marginVertical: 5 }}
+                    />
+                </ThemedView>
+
+                <ThemedView
+                    style={{
+                        marginHorizontal: 10,
+                        marginVertical: 5,
+                        flexDirection: "row",
+                        gap: 10,
+                    }}
+                >
+                    <ThemeTextInput placeholder="Precio" style={{ flex: 1 }} />
+                    <ThemeTextInput
+                        placeholder="Inventario"
+                        style={{ flex: 1 }}
+                    />
+                </ThemedView>
+            </ScrollView>
+        </KeyboardAvoidingView>
     );
 };
 
--- presentation/theme/components/ThemeTextInput.tsx
+++ presentation/theme/components/ThemeTextInput.tsx
@@ -1,14 +1,24 @@
 import { useColorScheme } from "@/presentation/theme/hooks/use-color-scheme";
 import { Ionicons } from "@expo/vector-icons";
 import { useRef, useState } from "react";
-import { StyleSheet, TextInput, TextInputProps, View } from "react-native";
+import {
+    StyleProp,
+    StyleSheet,
+    TextInput,
+    TextInputProps,
+    View,
+    ViewStyle,
+} from "react-native";
 import { useThemeColor } from "../hooks/use-theme-color";
 
-interface Props extends TextInputProps {
+interface Props extends Omit<TextInputProps, "style"> {
     icon?: keyof typeof Ionicons.glyphMap;
+    style?: StyleProp<ViewStyle>;
+    // Al usar Omit<TextInputProps, 'style'>, le dices a TypeScript: "Toma todas las propiedades de un input, excepto el estilo". Luego, añades style manualmente pero con el tipo ViewStyle, que es lo que el componente <View> de la línea 38 espera recibir
+    // por qué había un error en style, porque extendemos de TextInput y ahí solo admite style de textos, y nosotros colocamos los style en un View, no en un Text, entonces el View necesita styles de View
 }
 
-const ThemeTextInput = ({ icon, ...rest }: Props) => {
+const ThemeTextInput = ({ icon, style, ...rest }: Props) => {
     const primaryColor = useThemeColor({}, "primary");
     const textColor = useThemeColor({}, "text");
 
@@ -19,11 +29,14 @@ const ThemeTextInput = ({ icon, ...rest }: Props) => {
 
     return (
         <View
-            style={{
-                ...styles.border,
-                // ToDo: cambiar si tiene el foco el input
-                borderColor: isActive ? primaryColor : "#ccc",
-            }}
+            style={[
+                {
+                    ...styles.border,
+                    // ToDo: cambiar si tiene el foco el input
+                    borderColor: isActive ? primaryColor : "#ccc",
+                },
+                style,
+            ]}
             // esta siguiente linea es para que el input tenga el foco cuando se toca el borde el view
             onTouchStart={() => inputRef.current?.focus()}
         >
